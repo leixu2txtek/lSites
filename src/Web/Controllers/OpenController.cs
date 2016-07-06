d0c7d96774b8f27b9a16f626fa5346ed425e8769
leixu@txtek.cn <leixu@txtek.cn>
2016-07-06 19:17:12 +0800

    add get_categories,get_widgets action

--- src/Web/Controllers/OpenController.cs
+++ src/Web/Controllers/OpenController.cs
@@ -8,7 +8,7 @@ using System.Web;
 namespace Kiss.Components.Site.Web.Controllers
 {
     /// <summary>
-    /// 无需权限校验的控制器接口
+    /// 开放的控制器接口
     /// </summary>
     class OpenController : Controller
     {
@@ -35,7 +35,7 @@ namespace Kiss.Components.Site.Web.Controllers
         /// leixu
         /// 2016年6月30日11:19:05
         [HttpPost]
-        object gets(string[] ids)
+        object get_widgets(string[] ids)
         {
             var widgets = Widget.Gets(ids);
 
@@ -64,6 +64,54 @@ namespace Kiss.Components.Site.Web.Controllers
             {
                 code = 1,
                 widgets = data
+            };
+        }
+
+        /// <summary>
+        /// 获取站点下的栏目详细信息
+        /// </summary>
+        /// <remarks>请求方式：POST</remarks>
+        /// <param name="siteId">站点ID</param>
+        /// <returns>
+        /// {
+        ///     code = 1,                   //-1：指定的站点不存在，1：成功获取
+        ///     data = 
+        ///     [
+        ///         {
+        ///             id = "",            //栏目的ID
+        ///             title = "",         //栏目的标题
+        ///             url = "",           //栏目的Url
+        ///             parent_id = "",     //父级ID
+        ///             node_path = ""      //id路径
+        ///         }
+        ///     ]
+        /// }
+        /// </returns>
+        /// leixu
+        /// 2016年7月4日10:12:47
+        [HttpPost]
+        object get_categories(string siteId)
+        {
+            var site = Site.Get(siteId);
+
+            if (site == null) return new { code = -1, msg = "指定的站点不存在" };
+
+            var categories = (from q in Category.CreateContext()
+                              where q.SiteId == site.Id
+                              orderby q.SortOrder ascending
+                              select new
+                              {
+                                  id = q.Id,
+                                  title = q.Title,
+                                  url = q.Url,
+                                  parent_id = q.ParentId,
+                                  node_path = q.NodePath
+                              }).ToList();
+
+            return new
+            {
+                code = 1,
+                data = categories
             };
         }
     }
