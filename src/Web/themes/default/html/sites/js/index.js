6449434a570d3648fcbc59076a31bb45f1fc384d
leixu@txtek.cn <leixu@txtek.cn>
2016-07-21 14:53:46 +0800

    增加 表单编辑的提交示例代码

--- src/Web/themes/default/html/sites/js/index.js
+++ src/Web/themes/default/html/sites/js/index.js
@@ -1,13 +1,12 @@
-define(['../../../js/common'], function() {
+define(['../../../js/common'], function () {
 
     document.title = '站点列表 - 站群管理';
 
-    require(['template', 'moment', 'select2', 'form', 'paging', 'MDialog'], function(template, moment) {
+    require(['template', 'moment', 'select2', 'form', 'paging', 'MDialog'], function (template, moment) {
 
-        template.helper('format_date', function(date) {
+        template.helper('format_date', function (date) {
 
             return moment(date).format('YYYY-MM-DD');
-
         });
 
         var form = $('#site_form'),
@@ -18,57 +17,61 @@ define(['../../../js/common'], function() {
             allowClear: true
         });
 
-        if (form.find('input[name=siteId]').length == 0) {
-            form.append('<input type="hidden" name="siteId" value="' + util.get_query('siteId') + '" />');
-        };
-
-
         // 添加新站点    
-        $('.add-site', nav).on('click', function() {
+        $('.add-site', nav).on('click', function () {
 
-            var elems = $('#addSite').html();
+            var add_form = $(template('site_add_form', {})),
+                dlg = $M({
+                    title: '添加新站点',
+                    content: add_form[0],
+                    width: '450px',
+                    height: '350px',
+                    position: '50% 50%',
+                    ok: function () {
 
-            dlg = $M({
-                title: '添加新站点',
-                content: elems,
-                width: '450px',
-                height: '350px',
-                position: '50% 50%',
-                ok: function() {
-                    this.title('正在提交...');
+                        if (!confirm('是否确认添加站点？')) return false;
+
+                        add_form.submit();
+                    },
+                    okVal: '保存',
+                    cancel: false,
+                    cancelVal: '取消'
+                });
+
+            add_form.gform({
+                url: config.host + 'site/save',
+                beforeSubmit: function () {
+                    var title = $('[name=title]', add_form).val(),
+                        domain = $('[name=domain]', add_form).val();
+
+                    if (title.length == 0) {
+
+                        alert('站点名称不能为空');
+                        return false;
+                    }
+
+                    return false;
                 },
-                okVal: '保存',
-                cancel: false,
-                cancelVal: '取消'
+                onSuccess: function (r) {
+
+                    if (!r || r.code < 0) {
+
+                        alert(r.msg || '发生未知错误，请刷新后尝试');
+                        return false;
+                    }
+
+                    dlg.close();
+
+                    alert('已成功添加站点信息');
+                    form.submit();      //重新刷新站点列表
+                }
             });
-
-            var siteId = util.get_query('siteId'),
-                id = $(this).data('id'),
-                dlg = undefined,
-                site = function() {
-                    $.post(config.host + 'site/save'), {
-                            ids: [id],
-                            siteId: siteId,
-                        },
-                        function(r) {
-
-                            if (!r || r.code < 0) {
-                                alert(r.msg || '发生未知错误，请刷新尝试');
-                                return false;
-                            }
-
-                        }
-                };
         });
-
-
-
-
 
         //绑定表单
         form.gform({
             url: config.host + 'site/list',
-            onSuccess: function(r) {
+            onSuccess: function (r) {
 
                 if (!r || r.code < 0) {
 
@@ -83,16 +86,22 @@ define(['../../../js/common'], function() {
                 //绑定表格                
                 var table = $('table', form).gtable();
 
+                //TODO 绑定按钮事件
 
+                $('.edit', table).on('click', function () {
 
+                });
+
+                $('.delete', table).on('click', function () {
+
+                });
 
                 //绑定分页信息                
                 $('.x-paging-container', form).paging(r.paging);
             },
-            callback: function(form) {
+            callback: function (form) {
                 form.submit();
-            },
-
+            }
         });
     });
 });
